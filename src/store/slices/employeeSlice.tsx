b023fe7edd13c72142ebdc083c6356c990d989b7
Irsyaadf <muhammadirsyadfaddilah@gmail.com>
2025-02-17 09:47:45 +0700

    fix(docs): hapus dokumentasi lama & tambahkan use client

--- src/store/slices/employeeSlice.tsx
+++ src/store/slices/employeeSlice.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { Employee, EmployeeListResponse, EmployeeDetailResponse } from "@/types";
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { configureStore } from '@reduxjs/toolkit';
 import employeeReducer from './slices/employeeSlice';
 
